35226fb2bf906f5dabac24031a7869d309110358
Taher P <cooltaher552@gmail.com>
2021-10-25 19:48:06 +0530

    live port added

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express =require('express');
 const bodyParser =require('body-parser');
-
+var cors = require('cors');
 const {mongoose}= require('./server.js')
 var CURD =require('./controllers/CURD.js')
 var matchCURD=require('./controllers/matchCURD')
 
 var app= express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, Authorization");
+    if (req.method === 'OPTIONS') {
+        req.header('Access-Control-Allow-Methods', 'PUT,PATCH,POST,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/data', CURD);
 app.use('/match',matchCURD)
 
-app.listen(3000,()=>console.log('server started at port :3000')); 
+app.listen(process.env.PORT || 3000,()=>console.log('server started at port :3000')); 
